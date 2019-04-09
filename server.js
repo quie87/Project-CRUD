dc1deec832a5e40b56e17dd30b6c9419cd72e33b
Kristoffer <kristoffer.aberg12@gmail.com>
2019-04-09 18:51:33 +0200

    adding helmet module for security

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const helmet = require('helmet')
+
+const port = process.env.PORT || 5000
 
 const app = express()
 
 // Bodyparser middleware
 app.use(bodyParser.json())
 
-const port = process.env.PORT || 5000
+// Put the helmet on
+app.use(helmet())
 
-app.listen(port, () => console.log(`Server started on port: ${port}`))
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    // defaultSrc: ["'self'"],
+    // fontSrc: ["'self'"],
+    // styleSrc: ["'self'"],
+    scriptSrc: ["'self'"]
+  }
+}))
+
+// Starts server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+  console.log('Press Ctrl-C to terminate...\n')
+})
