195bce70a16585ded595e41f1b0049f824781990
Kristoffer <kristoffer.aberg12@gmail.com>
2019-04-12 17:02:06 +0200

    adding todos with static items for now

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react'
+import Header from './components/layout/header'
+import Todos from './components/Todos/Todos'
+import uuid from 'uuid'
+
 import './App.css'
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: uuid(),
+        title: 'Get this to work',
+        completed: false
+      },
+      {
+        id: uuid(),
+        title: 'Make dinner',
+        completed: false
+      },
+      {
+        id: uuid(),
+        title: 'Go to sleep',
+        completed: false
+      },
+    ]
+  }
+
+  toggleComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    }) })
+  }
+  
+  deleteTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+  }
+
   render () {
     return (
       <div className='App'>
+        <Header />
         <h1>Hello World!!</h1>
-
+        <Todos todos={this.state.todos} toggleComplete={this.toggleComplete} deleteTodo={this.deleteTodo} />
       </div>
     )
   }
---
+++ client/src/components/layout/header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render () {
+    return (
+      <div className='nav-bar'>
+        <ol>
+          <li>
+						Home
+          </li>
+          <li>
+						Register
+          </li>
+          <li>
+						login
+          </li>
+        </ol>
+      </div>
+    )
+  }
+}
+
+export default Header
