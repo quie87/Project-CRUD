e5e0cc251e9449228755485237f2a14ef0c81e90
Kristoffer <kristoffer.aberg12@gmail.com>
2019-05-10 16:49:32 +0200

    serving static assets in production

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const mongoose = require('./config/mongoose.js')
 const helmet = require('helmet')
+const path = require('path')
 require('dotenv').config()
 
 const port = process.env.PORT || 5000
+
+const app = express()
 
 // Connect to the database.
 mongoose.run().catch(error => {
   console.error(error)
   process.exit(1)
 })
-
-const app = express()
 
 // Bodyparser middleware
 app.use(express.json())
@@ -32,6 +33,16 @@ app.use('/api/projects', require('./routes/api/projects'))
 app.use('/api/todos', require('./routes/api/todos'))
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 // Starts server
 app.listen(port, () => {
