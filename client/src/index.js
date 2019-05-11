e224efb094d0d82c5708174a4bc6deb62be257ef
Kristoffer <kristoffer.aberg12@gmail.com>
2019-05-11 16:10:54 +0200

    disable serviceworker for test

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import App from './App'
 
 import { Provider } from 'react-redux'
 import store from './store'
-import * as serviceWorker from './serviceWorker'
+// import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -12,7 +12,7 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'))
 
-serviceWorker.register()
+// serviceWorker.register()
 
 // funkar typ
 // function register () {
