a6b5b0cf48705cb0cddd3a8ff8dc14dbcca92fa4
Kristoffer <kristoffer.aberg12@gmail.com>
2019-05-12 11:48:02 +0200

    refactor test

--- server.js
+++ server.js
@@ -63,8 +63,4 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 5000
 
-console.log(process.env)
-
 app.listen(port, () => console.log(`Server started on port ${port}`))
-
-// module.exports = app
