f563adbcb3a1c710a6ac2e28ad30f5bfdcba02e2
Kristoffer <kristoffer.aberg12@gmail.com>
2019-05-16 19:45:52 +0200

    testing sw

--- client/public/sw_cache_site.js
+++ client/public/sw_cache_site.js
@@ -1,23 +1,23 @@
 const cacheName = 'v1'
 
-// const urlsToPrefetch = [
-//   '/api/auth/user',
-//   '/static/css/main.9798b67c.chunk.css',
-//   '/static/js/main.72205561.chunk.js'
-// ]
+const urlsToPrefetch = [
+  '/index.html',
+  '/manifest.json',
+  '/icons/favicon.ico'
+]
 // Call install event
 self.addEventListener('install', (e) => {
   console.log('Service Worker: Installed')
 
-  // e.waitUntil(
-  //   caches
-  //     .open(cacheName)
-  //     .then(cache => {
-  //       console.log('Service worker caching files')
-  //       cache.add(urlsToPrefetch)
-  //     })
-  //     .then(() => self.skipWaiting())
-  // )
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log('Service worker caching files')
+        cache.add(urlsToPrefetch)
+      })
+      .then(() => self.skipWaiting())
+  )
 })
 
 // Call activate event
@@ -56,44 +56,21 @@ self.addEventListener('activate', (e) => {
 //   )
 // })
 
-// self.addEventListener('fetch', event => {
-//   console.log('service worker: fetching...')
-
-//   if (event.request.method !== 'GET') return
-
-//   event.respondWith(
-//     caches.open(cacheName)
-//       .then(cache => {
-//         return cache.match(event.request).then(cacheResponse => {
-//           const fetchPromise = fetch(event.request).then(networkResponse => {
-//             cache.put(event.request, networkResponse.clone())
-//             return networkResponse
-//           })
-//           return cacheResponse || fetchPromise
-//         })
-//       }).catch(() => console.log('You are offline, serving cached content'))
-//   )
-// })
-
 self.addEventListener('fetch', event => {
-  // Let the browser do its default thing
-  // for non-GET requests.
+  console.log('service worker: fetching...')
+
   if (event.request.method !== 'GET') return
 
-  // Prevent the default, and handle the request ourselves.
-  event.respondWith(async function () {
-    // Try to get the response from a cache.
-    const cache = await caches.open(cacheName)
-    const cachedResponse = await cache.match(event.request)
-
-    if (cachedResponse) {
-      // If we found a match in the cache, return it, but also
-      // update the entry in the cache in the background.
-      event.waitUntil(cache.add(event.request))
-      return cachedResponse
-    }
-
-    // If we didn't find a match in the cache, use the network.
-    return fetch(event.request)
-  }())
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.match(event.request).then(cacheResponse => {
+          const fetchPromise = fetch(event.request).then(networkResponse => {
+            cache.put(event.request, networkResponse.clone())
+            return networkResponse
+          })
+          return cacheResponse || fetchPromise
+        })
+      }).catch(() => console.log('You are offline, serving cached content'))
+  )
 })
