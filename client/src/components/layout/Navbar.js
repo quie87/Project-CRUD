40107c603d7316f8a143356f702c08ef60d0f6dd
Kristoffer <kristoffer.aberg12@gmail.com>
2019-05-17 20:29:10 +0200

    changing my hole Navbar, excluding the signedin/signdout link files

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-import SignedInLinks from './SignedInLinks'
-import SignedOutLinks from './SignedOutLinks'
-import { Navbar as Navbars } from 'react-materialize'
+// import SignedInLinks from './SignedInLinks'
+// import SignedOutLinks from './SignedOutLinks'
+// import { Navbar as Navbars } from 'react-materialize'
+import { logout } from '../../actions/authActions'
 
 class Navbar extends Component {
   render () {
     const { isAuthenticated } = this.props.auth
     return (
-      <Navbars brand={<Link to='/'>Project Planner</Link>} alignLinks='right' className='Navbar #ff7043 deep-orange lighten-1' >
-        { isAuthenticated ? <SignedInLinks /> : <SignedOutLinks />}
-      </Navbars>
-      // <Navbars brand={<Link to='/'>Project Planner</Link>} alignLinks='right' className='Navbar #ff7043 deep-orange lighten-1'>
-      //   <Link to='/signUp'>signUp</Link>
-      //   <Link to='/signIn'>signIp</Link>
-      // </Navbars>
+    // <Navbars brand={<Link to='/'>Project Planner</Link>} alignLinks='right' className='Navbar #ff7043 deep-orange lighten-1' >
+    //   { isAuthenticated ? <SignedInLinks /> : <SignedOutLinks />}
+    // </Navbars>
+      <div>
+        <nav>
+          <div className='nav-wrapper #ff7043 deep-orange lighten-1'>
+            <a href='/' className='brand-logo'>Project Planner</a>
+            <a href='#!' data-target='mobile-nav' className='sidenav-trigger'><i className='material-icons'>menu</i></a>
+            {isAuthenticated
+              ? <ul className='right hide-on-med-and-down'>
+                <li><a href='/' onClick={this.props.logout}>Log out</a></li>
+              </ul>
+              : <ul className='right hide-on-med-and-down'>
+                <li><a href='signUp'>SignUp</a></li>
+                <li><a href='signIn'>SignIn</a></li>
+              </ul>
+            }
+          </div>
+        </nav>
+
+        {isAuthenticated
+          ? <ul className='sidenav' id='mobile-nav'>
+            <li><a href='/' onClick={this.props.logout}>Log out</a></li>
+          </ul>
+          : <ul className='sidenav' id='mobile-nav'>
+            <li><a href='signUp'>SignUp</a></li>
+            <li><a href='signIn'>SignIn</a></li>
+          </ul>
+        }
+      </div>
+
     )
   }
 }
@@ -26,5 +51,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  null
+  { logout }
 )(Navbar)
