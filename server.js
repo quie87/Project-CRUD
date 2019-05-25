42e336df9ad08f59508019c4408c5354b8f423af
Kristoffer <kristoffer.aberg12@gmail.com>
2019-05-25 13:40:45 +0200

    changing CSP

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(helmet())
 app.use(helmet.contentSecurityPolicy({
   directives: {
     defaultSrc: ["'self'"],
-    fontSrc: ["'self'", 'cdnjs.cloadflare.com', 'https://fonts.googleapis.com'],
+    fontSrc: ["'self'", 'cdnjs.cloadflare.com', 'https://fonts.googleapis.com', 'https://fonts.gstatic.com/s/materialicons'],
     styleSrc: ["'self'", "'unsafe-inline'", 'cdnjs.cloadflare.com', 'https://fonts.googleapis.com/icon?family=Material+Icons'],
     scriptSrc: ["'self'", "'unsafe-inline'", 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js']
   }
