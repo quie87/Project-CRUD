2dca7227522188f9b373ca5e9b21bc67cb27ea5c
Kristoffer <kristoffer.aberg12@gmail.com>
2019-05-26 15:11:50 +0200

    changing text and layout on landing page

--- client/src/components/dashboard/LandingPage.js
+++ client/src/components/dashboard/LandingPage.js
@@ -1,50 +1,43 @@
 import React, { Component } from 'react'
 import SignUp from '../auth/SignUp'
-import computer from '../../images/computer.png'
 
 class LandingPage extends Component {
   render () {
     return (
       <div className='row innerWrapper'>
-        <div >
-          <img src={computer} alt='phone' className='landing-img col s12 m8 l6' />
-        </div>
-        <div className='signup col s12 m4 l6' >
-          <SignUp />
-        </div>
-
-        <div className='col s12 m12 l12 center'>
-          <div className='main'>
+        <div className='box-left col s12 m8 l6'>
+          <div className='box1'>
             <h1>
-              Keep it simple, don't forget
+				Keep It Simple, <br />
+				Get Stuff Done
             </h1>
+            <p>
+				Get Stuff Done is what you need to plan, organize and to get the sweet satisfaction of
+				checking all your stuff off once you´re done! From projects to shopping lists, Get Stuff done
+				will make your life easier whenever and wherever you are. <br />
+				All at the same place with an easy layout.
+            </p>
           </div>
-          <div className='boxes'>
-            <div className='box1 col s12 m6 l6'>
-              <p>
-                Project planner is now available to help you plan and structure your projects and tasks.
-                Make your life easier by having all your planning in one reliable place that is allways available. <br />
-
-                No more frustration over slow or no internet connection.
-                Project planner makes it possible for you to keep navigating thrue your projects, even when offline!
-              </p>
-            </div>
-            <div className='box2 col s12 m6 l6'>
-              <ul>
-                <li>
-                    Easy overview of your projects and tasks
-                </li>
-                <li>
-                    Can be used on desktops, tablets and phones
-                </li>
-                <li>
-                    You can navigate thrue your projects, even when offline!
-                </li>
-                <li>
-                    Possible to shortcut icon on your phone or tablet
-                </li>
-              </ul>
-            </div>
+          <div className='box2'>
+            <ul>
+              <li>
+                    Easy overview of your projects, lists and tasks
+              </li>
+              <li>
+                    Whenever; online or offline
+              </li>
+              <li>
+                    Wherever; on your desktop, tablet and phone
+              </li>
+              <li>
+                    Easy access, just a push on an icon and you can start getting stuff done
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='box-right col s12 m4 l6'>
+          <div className='box2 signup' >
+            <SignUp />
           </div>
         </div>
       </div>
