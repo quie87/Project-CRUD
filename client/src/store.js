667a33457d2bcdbf5ed37b682262863b4f8050e3
Kristoffer <kristoffer.aberg12@gmail.com>
2019-05-29 12:24:37 +0200

    setup heroku

--- client/src/store.js
+++ client/src/store.js
@@ -7,14 +7,12 @@ const initialState = {}
 const middleware = [ thunk ]
 
 // Creates store and make it possible to use the Redux devtools in the browser
-const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    process.env.NODE_ENV !== 'production' && reduxDevTools
+    applyMiddleware(...middleware)
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   ))
 
