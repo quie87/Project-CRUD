eb0311fa1cd9c8beeb15a156b3cec368b040faa6
Kristoffer <kristoffer.aberg12@gmail.com>
2019-05-29 12:28:26 +0200

    fixed a typo

--- client/public/sw_cache_site.js
+++ client/public/sw_cache_site.js
@@ -29,9 +29,11 @@ self.addEventListener('fetch', e => {
 
   e.respondWith(
     fetch(e.request).then(res => {
+      const resClone = res.clone()
       caches.open(cacheName)
         .then(cache => {
-          cache.put(e.request, res.clone())
+          // Add response to cache
+          cache.put(e.request, resClone)
         })
       return res
     })
