3c82d24c6581556c16add4aa24e7232003bd2dce
Kristoffer <kristoffer.aberg12@gmail.com>
2019-05-29 11:03:18 +0200

    Testing new code for service worker

--- client/public/sw_cache_site.js
+++ client/public/sw_cache_site.js
@@ -26,7 +26,19 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', event => {
   console.log('service worker: fetching...')
 
-  if (event.request.method !== 'GET') return
+  // if (event.request.method !== 'GET') return
+  if (event.request.method !== 'GET') {
+    event.respondWith(
+      caches.open(cacheName)
+        .then(cache => {
+          const fetchPromise = fetch(event.request).then(networkResponse => {
+            cache.put(event.request, networkResponse.clone())
+            return networkResponse
+          }).catch(() => console.log('You are offline, can´t add or delete items at this point, try again when you get connection'))
+          return fetchPromise
+        })
+    )
+  }
 
   event.respondWith(
     caches.open(cacheName)
